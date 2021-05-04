d0373c915da75c71152d8e56b9caddff23422241
mifrok <mifra@mail.ru>
2021-05-04 13:51:56 +0700

    homework_2_dynamic_title_part

--- lesson_1/geekshop/mainapp/views.py
+++ lesson_1/geekshop/mainapp/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 
+# title = [{'index': 'Магазин'},
+#          {'products': 'Каталог'},
+#          {'contact': 'Контакты'}]
+links = [{'href': 'index', 'title': 'Магазин'},
+        {'href': 'products', 'title': 'Каталог'},
+        {'href': 'contacts', 'title': 'Контакты'}]
+content = {
+    'titles': links,
+}
+
 
 def main(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', context=content)
 
 
 def products(request):
-    return render(request, 'products.html')
+    return render(request, 'products.html', context=content)
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'contact.html', context=content)
 
 # Create your views here.
