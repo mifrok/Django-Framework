597bbcf4acc0e47814673b208331fccf9470f4a9
mifrok <mifra@mail.ru>
2021-05-16 18:21:59 +0700

    lesson_6_basket_update

--- lesson_1/geekshop/basketapp/views.py
+++ lesson_1/geekshop/basketapp/views.py
@@ -4,27 +4,26 @@ from mainapp.models import Product
 
 
 def basket(request):
+    title = 'корзина'
+    basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
     content = {
-        'basket': Basket.objects.all()
+        'title': title,
+        'basket_items': basket_items,
     }
     return render(request, 'basket.html', content)
 
 
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
-
     basket = Basket.objects.filter(user=request.user, product=product).first()
-
     if not basket:
         basket = Basket(user=request.user, product=product)
-
     basket.quantity += 1
     basket.save()
-
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 def basket_remove(request, pk):
-    content = {}
-    return render(request, 'basket.html', content)
-
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
