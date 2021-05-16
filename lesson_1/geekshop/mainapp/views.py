0b2301eb74a357111b0f03829471c5545cbedc70
mifrok <mifra@mail.ru>
2021-05-16 18:14:11 +0700

    lesson_6_hot_products

--- lesson_1/geekshop/mainapp/views.py
+++ lesson_1/geekshop/mainapp/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 from .models import ProductCategory, Product
 from django.shortcuts import get_object_or_404
@@ -19,6 +21,26 @@ content = {
 }
 
 
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category). \
+                        exclude(pk=hot_product.pk)[:3]
+
+    return same_products
+
+
 def main(request):
     return render(request, 'index.html', context=content)
 
@@ -26,7 +48,7 @@ def main(request):
 def products(request, pk=None):
     title = 'продукты'
     links_menu = ProductCategory.objects.all()
-    basket = []
+    basket = get_basket(request.user)
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
 
@@ -48,13 +70,17 @@ def products(request, pk=None):
 
         return render(request, 'products_list.html', content)
 
-    same_products = Product.objects.all()[3:5]
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
 
     content = {
         'title': title,
         'links_menu': links_menu,
         'same_products': same_products,
         'old_links': links,
+        'hot_product': hot_product,
+        'basket': basket,
+
     }
     return render(request, 'products.html', context=content)
 
