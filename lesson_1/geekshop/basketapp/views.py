38ae1a72fbe5f4665a1efbeb1b20d83d8fcb15ae
mifrok <mifra@mail.ru>
2021-05-16 19:15:52 +0700

    lesson_6_AJAX

--- lesson_1/geekshop/basketapp/views.py
+++ lesson_1/geekshop/basketapp/views.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from basketapp.models import Basket
+from django.template.loader import render_to_string
+from django.urls import reverse
 from mainapp.models import Product
 
 
+@login_required
 def basket(request):
     title = 'корзина'
     basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
@@ -13,7 +18,10 @@ def basket(request):
     return render(request, 'basket.html', content)
 
 
+@login_required
 def basket_add(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
     if not basket:
@@ -23,7 +31,31 @@ def basket_add(request, pk):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, pk, quantity):
+    if request.is_ajax():
+        quantity = int(quantity)
+        new_basket_item = Basket.objects.get(pk=int(pk))
+
+        if quantity > 0:
+            new_basket_item.quantity = quantity
+            new_basket_item.save()
+        else:
+            new_basket_item.delete()
+
+        basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+
+        content = {
+            'basket_items': basket_items,
+        }
+
+        result = render_to_string('basketapp/includes/inc_basket_list.html', content)
+
+        return JsonResponse({'result': result})
